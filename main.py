bdf72c99c96ef6172245a7b73128c30a3bb967a4
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-28 11:00:23 -0500

    nix wolverine

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fasthtml.common import serve, fast_app
+from fasthtml.common import serve, fast_app, Titled
 from fh_altair import altair_headers
 
-from wolverine import Wolverine
+from blocks import Index, Components, Prices, wrap
 from style import styles
 
 app, rt = fast_app(live=True, debug=True, hdrs=[styles, altair_headers])
@@ -9,7 +9,12 @@ app, rt = fast_app(live=True, debug=True, hdrs=[styles, altair_headers])
 
 @rt
 def index():
-    return Wolverine()
+    return Titled(
+        "Michigan Consumer Sentiment Index",
+        wrap(*Index()),
+        wrap(*Components()),
+        wrap(*Prices()),
+    )
 
 
 serve()
